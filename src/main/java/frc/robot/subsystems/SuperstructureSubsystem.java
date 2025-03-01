6ba4c4a9977803fdb10ef2dd630a742b17d59fce
Yasushiko <yasushiko.xd@gmail.com>
2025-03-01 12:22:13 -0500

    Not Connected?

--- src/main/java/frc/robot/subsystems/SuperstructureSubsystem.java
+++ src/main/java/frc/robot/subsystems/SuperstructureSubsystem.java
@@ -1,28 +1,21 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.FeedbackConfigs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.controls.MotionMagicDutyCycle;
-import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class SuperstructureSubsystem extends SubsystemBase {
+public final class SuperstructureSubsystem extends SubsystemBase {
 
-    //TODO: find ids
+    private final TalonFX L_elevatorMotor = new TalonFX(1,"rio");
+    private final TalonFX R_elevatorMotor = new TalonFX(2, "rio");
 
-    private final TalonFX L_elevatorMotor = new TalonFX(25);
-    private final TalonFX R_elevatorMotor = new TalonFX(24);
+    public final double ElevatorP = 0.01;
+    public final double ElevatorI = 0;
+    public final double ElevatorD = 0;
 
-    private final double MAX_VELOCITY = 0;
-    private final double MAX_ACCELERATION = 0;
-
-    private double ElevatorP = 0.01;
-    private double ElevatorI = 0;
-    private double ElevatorD = 0;
-
-    private final MotionMagicDutyCycle motionMagicDutyCycle = new MotionMagicDutyCycle(0);
    
 
     public SuperstructureSubsystem() {
@@ -30,34 +23,21 @@ public class SuperstructureSubsystem extends SubsystemBase {
     }
 
     public void configElevator() {
-        TalonFXConfiguration leftConfiguration = new TalonFXConfiguration();
-        TalonFXConfiguration rightConfiguration = new TalonFXConfiguration();
+        var slot0Configs = new Slot0Configs();
         FeedbackConfigs L_feedbackConfigs = new FeedbackConfigs();
-        
-        leftConfiguration.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-        rightConfiguration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
 
-        L_elevatorMotor.setNeutralMode(NeutralModeValue.Brake);
-        R_elevatorMotor.setNeutralMode(NeutralModeValue.Brake);
+        R_elevatorMotor.setNeutralMode(NeutralModeValue.Coast);
+        L_elevatorMotor.setNeutralMode(NeutralModeValue.Coast);
     
-        leftConfiguration.Slot0.kP = ElevatorP;
-        leftConfiguration.Slot0.kI = ElevatorI;
-        leftConfiguration.Slot0.kD = ElevatorD;
+        slot0Configs.kP = ElevatorP;
+        slot0Configs.kI = ElevatorI;
+        slot0Configs.kD = ElevatorD;
 
-        L_feedbackConfigs.RotorToSensorRatio = 1; //! YOUYR GEAR RATIO
-        //TODOL: FIND THE PROPER ONE AND SET IOT IN THERE ^^^^^^
-        // WE WANT THE BIKINI BOTTOM TO BE 0 AND TOP 1000 BADABING BADABOOM
-        // MAX WILL BE 3450122
-        // 1000/3450122  = 0.0002898 <- this is the ratio
-        // 0.0002898 * 3450122 = 1000 <- this is the max
-        
+        L_feedbackConfigs.RotorToSensorRatio = 1;
 
-        L_elevatorMotor.getConfigurator().apply(leftConfiguration);
-        R_elevatorMotor.getConfigurator().apply(rightConfiguration);
+        L_elevatorMotor.getConfigurator().apply(slot0Configs);
+        R_elevatorMotor.getConfigurator().apply(slot0Configs);
         L_elevatorMotor.getConfigurator().apply(L_feedbackConfigs);
-
-        leftConfiguration.MotionMagic.MotionMagicCruiseVelocity = MAX_VELOCITY;
-        leftConfiguration.MotionMagic.MotionMagicAcceleration = MAX_ACCELERATION;
 
         R_elevatorMotor.setControl(new com.ctre.phoenix6.controls.Follower(L_elevatorMotor.getDeviceID(), true));
     
@@ -73,7 +53,6 @@ public class SuperstructureSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
-        System.out.println("Elevator Position: " + getElevatorPosition());
-        // setElevatorPosition(1000);
+        System.out.println(L_elevatorMotor.isConnected());
     }
 }
